7cea616e59d197ea2196b6ed827d8feb3870dfae
zhdc <995949180@qq.com>
2018-06-04 15:54:54 +0800

    a

---
+++ wwwroot/phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhudechao
+ * Date: 2018/5/19
+ * Time: 下午8:10
+ */
+
+phpinfo();
---
+++ wwwroot/static/js/vue/vuetest_index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zhudechao on 2018/5/19.
+ */
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
