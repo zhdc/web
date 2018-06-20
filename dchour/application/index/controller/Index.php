b9ff0d46e0bb6f26dbeb83aaf08114c80660b2af
zhdc <995949180@qq.com>
2018-06-20 16:53:47 +0800

    dchour

---
+++ dchour/application/index/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\index\controller;
+
+
+use think\Config;
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function hello($name = 'ThinkPHP5')
+    {
+        return 'hello,' . $name;
+    }
+}
