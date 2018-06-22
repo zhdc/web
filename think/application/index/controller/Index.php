f320e336abd9ac68e35047c1353d4f1a03bb9a28
zhdc <995949180@qq.com>
2018-06-22 10:42:43 +0800

    a

--- think/application/index/controller/Index.php
+++ think/application/index/controller/Index.php
@@ -8,11 +8,7 @@ class Index
 {
     public function index()
     {
-<<<<<<< HEAD
         return "ThinkPHP 5.1.16";
-=======
-        echo \config('app_name');
->>>>>>> 010a10360a08e0f932ace339bdcca7ad19fc7187
     }
 
     public function hello($name = 'ThinkPHP5')
