842180bbaf39d04e8e682107f98fe150317e70a2
zhdc <995949180@qq.com>
2018-09-04 21:38:55 +0800

    图片base64

---
+++ dchour/extend/zhdc/image/ImgBase64.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhudechao
+ * Date: 2018/9/4
+ * Time: 下午9:07
+ */
+namespace zhdc\image;
+class ImgBase64
+{
+    /**
+     * @param $image_file 图片位置
+     * @return string
+     */
+    public static function base64EncodeImage ($image_file) {
+        $base64_image = '';
+        $image_info = getimagesize($image_file);
+        $image_data = fread(fopen($image_file, 'r'), filesize($image_file));
+        $base64_image = 'data:' . $image_info['mime'] . ';base64,' . chunk_split(base64_encode($image_data));
+        return $base64_image;
+    }
+
+    public static function base64_image_content($base64_image_content,$path){
+        //匹配出图片的格式
+        if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image_content, $result)){
+            $type = $result[2];
+            $new_file = $path."/".date('Ymd',time())."/";
+            if(!file_exists($new_file)){
+                //检查是否有该文件夹，如果没有就创建，并给予最高权限
+                mkdir($new_file, 0777);
+            }
+            $new_file = $new_file.time().".{$type}";
+            if (file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_image_content)))){
+                return '/'.$new_file;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+}
